482566d5614d3406c4b7d4fa0cfc865e5a58146f
Joe Ferroggiaro <jfferroggiaro@gmail.com>
2019-09-13 09:25:48 -0700

    Added Math class

---
+++ Math.java
@@ -0,0 +1,12 @@
+public class Math{
+  public static void main(String[] args){
+
+    int num;
+    double val;
+
+    num = 17 / 2.0 + 4;
+
+    System.out.println("num = " + num);
+    //System.out.println("val = " + val);
+  }
+}
