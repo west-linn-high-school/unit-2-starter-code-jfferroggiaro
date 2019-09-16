18867843b80bed05de17ca53c1cd97ca7f5557ed
Joe Ferroggiaro <jfferroggiaro@gmail.com>
2019-09-16 09:23:46 -0700

    Added Geometry, Quadratic Roots, Average

---
+++ Geometry.java
@@ -0,0 +1,11 @@
+public class Geometry{
+  public static void main(String[] args){
+    int sides = 7;
+    int radius = 5;
+    final double pi = 3.14;
+
+    System.out.println("A heptagon has " + sides + " sides.");
+    System.out.println("A decagon has " + radius*2 + " sides.");
+    System.out.println();
+  }
+}
---
+++ QuadraticRoots.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+
+public class QuadraticRoots{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("What's your name?");
+    String name = input.next();
+
+    System.out.println("Hello " + name);
+    System.out.println("What are the coefficients of the Quadratic?");
+    int num1 = input.nextInt();
+    int num2 = input.nextInt();
+    int num3 = input.nextInt();
+
+
+
+  }
+}
